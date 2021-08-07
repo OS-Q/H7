e5f532595174e9d87364889cafd9728852110cbb
qitas <qitas@qitas.cn>
2021-08-07 18:53:54 +0800

    fix

--- examples/arduino/blink/Src/Blink.cpp
+++ examples/arduino/blink/Src/Blink.cpp
@@ -8,11 +8,13 @@
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
 // #define LED_BUILTIN 2
+uint32_t chipId = 0;
 
 void setup()
 {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop()
@@ -25,4 +27,10 @@ void loop()
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
   delay(1000);
+  for(int i=0; i<17; i=i+8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+	}
+	Serial.printf("ESP32 Chip model = %s Rev %d\n", ESP.getChipModel(), ESP.getChipRevision());
+	Serial.printf("This chip has %d cores\n", ESP.getChipCores());
+  Serial.print("Chip ID: "); Serial.println(chipId);
 }
