4c1745a209d4d14e08e09a780a70ea0d3ed48814
qitas <qitas@qitas.cn>
2021-08-07 17:47:18 +0800

    test

--- examples/espidf/espidf-aws-iot/src/subscribe_publish_sample.c
+++ examples/espidf/espidf-aws-iot/src/subscribe_publish_sample.c
@@ -1,28 +1,4 @@
-/*
- * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * Additions Copyright 2016 Espressif Systems (Shanghai) PTE LTD
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-/**
- * @file subscribe_publish_sample.c
- * @brief simple MQTT publish and subscribe on the same topic
- *
- * This example takes the parameters from the build configuration and establishes a connection to the AWS IoT MQTT Platform.
- * It subscribes and publishes to the same topic - "test_topic/esp32"
- *
- * Some setup is required. See example README for details.
- *
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
