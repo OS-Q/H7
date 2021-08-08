b43e70b6e16d2c04895bd33ea667396cfca0b843
qitas <qitas@qitas.cn>
2021-08-08 14:35:20 +0800

    try

--- examples/espidf/espidf-arduino-blink/src/Blink.cpp
+++ examples/espidf/espidf-arduino-blink/src/Blink.cpp
@@ -1,9 +1,3 @@
-/* Blink Example
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
@@ -23,6 +17,12 @@
 
 void blink_task(void *pvParameter)
 {
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
